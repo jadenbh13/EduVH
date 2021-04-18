39c9fdf6607a447bcca9e70e8d7e8d57b5169738
jadenbh13 <68014752+jadenbh13@users.noreply.github.com>
2021-04-18 10:35:39 -0400

    Update utils.py

--- utils.py
+++ utils.py
@@ -48,19 +48,9 @@ def epoch(data, samples_epoch, samples_overlap=0):
 
 
 def compute_band_powers(eegdata, fs):
-    """Extract the features (band powers) from the EEG.
-    Args:
-        eegdata (numpy.ndarray): array of dimension [number of samples,
-                number of channels]
-        fs (float): sampling frequency of eegdata
-    Returns:
-        (numpy.ndarray): feature matrix of shape [number of feature points,
-            number of different features]
-    """
-    # 1. Compute the PSD
+
     winSampleLength, nbCh = eegdata.shape
 
-    # Apply Hamming window
     w = np.hamming(winSampleLength)
     dataWinCentered = eegdata - np.mean(eegdata, axis=0)  # Remove offset
     dataWinCenteredHam = (dataWinCentered.T * w).T
@@ -70,18 +60,13 @@ def compute_band_powers(eegdata, fs):
     PSD = 2 * np.abs(Y[0:int(NFFT / 2), :])
     f = fs / 2 * np.linspace(0, 1, int(NFFT / 2))
 
-    # SPECTRAL FEATURES
-    # Average of band powers
-    # Delta <4
+
     ind_delta, = np.where(f < 4)
     meanDelta = np.mean(PSD[ind_delta, :], axis=0)
-    # Theta 4-8
     ind_theta, = np.where((f >= 4) & (f <= 8))
     meanTheta = np.mean(PSD[ind_theta, :], axis=0)
-    # Alpha 8-12
     ind_alpha, = np.where((f >= 8) & (f <= 12))
     meanAlpha = np.mean(PSD[ind_alpha, :], axis=0)
-    # Beta 12-30
     ind_beta, = np.where((f >= 12) & (f < 30))
     meanBeta = np.mean(PSD[ind_beta, :], axis=0)
 
@@ -94,9 +79,7 @@ def compute_band_powers(eegdata, fs):
 
 
 def nextpow2(i):
-    """
-    Find the next power of 2 for number i
-    """
+
     n = 1
     while n < i:
         n *= 2
@@ -104,15 +87,12 @@ def nextpow2(i):
 
 
 def compute_feature_matrix(epochs, fs):
-    """
-    Call compute_feature_vector for each EEG epoch
-    """
+
     n_epochs = epochs.shape[2]
 
     for i_epoch in range(n_epochs):
         if i_epoch == 0:
             feat = compute_band_powers(epochs[:, :, i_epoch], fs).T
-            # Initialize feature_matrix
             feature_matrix = np.zeros((n_epochs, feat.shape[0]))
 
         feature_matrix[i_epoch, :] = compute_band_powers(
@@ -122,12 +102,7 @@ def compute_feature_matrix(epochs, fs):
 
 
 def get_feature_names(ch_names):
-    """Generate the name of the features.
-    Args:
-        ch_names (list): electrode names
-    Returns:
-        (list): feature names
-    """
+
     bands = ['delta', 'theta', 'alpha', 'beta']
 
     feat_names = []
@@ -139,10 +114,7 @@ def get_feature_names(ch_names):
 
 
 def update_buffer(data_buffer, new_data, notch=False, filter_state=None):
-    """
-    Concatenates "new_data" into "data_buffer", and returns an array with
-    the same size as "data_buffer"
-    """
+
     if new_data.ndim == 1:
         new_data = new_data.reshape(-1, data_buffer.shape[1])
 
@@ -160,10 +132,7 @@ def update_buffer(data_buffer, new_data, notch=False, filter_state=None):
 
 
 def get_last_data(data_buffer, newest_samples):
-    """
-    Obtains from "buffer_array" the "newest samples" (N rows from the
-    bottom of the buffer)
-    """
+
     new_buffer = data_buffer[(data_buffer.shape[0] - newest_samples):, :]
 
     return new_buffer
